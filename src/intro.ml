71657b6d82df7e739a02c91d381324dab0bb5f4e
David Scott <dave.scott@eu.citrix.com>
2012-12-02 12:59:05 +0000

    More talk updates

--- src/intro.ml
+++ src/intro.ml
@@ -48,6 +48,32 @@ let slides = [
 {
   styles=[];
   content= <:html<
+    <h3>Talk structure</h3>
+    <ol>
+    <li><b>XenServer and OCaml: a brief history</b>
+      <ul>
+      <li>What is XenServer? </li>
+      <li> Where is the OCaml? </li>
+      </ul>
+    </li>
+    <li>Low-level systems programming in OCaml
+      <ul>
+      <li>OCaml in kernel space;</li>
+      <li>Run OCaml directly on the cloud</li>
+      </ul>
+    </li>
+    <li>Future XenServer architecture
+      <ul>
+      <li>Where OCaml fits</li>
+      </ul>
+    </li>
+  </ol>
+  >>;
+};
+
+{
+  styles=[];
+  content= <:html<
     <h3>What is XenServer?</h3>
     <ul>
       <li>a hypervisor platform</li>
@@ -78,7 +104,13 @@ let slides = [
   content= <:html<
     <h3>Why did we choose OCaml??!</h3>
     <ul>
-    <li> We had:
+    <li> Back in 2006 we were:
+      <ul>
+      <li> XenSource: a VC-funded spin-off from the <a href="http://www.cl.cam.ac.uk/">University of Cambridge Computer Lab</a></li>
+      <li> Building software to manage datacenters running the <a href="http://www.xen.org/">xen</a> hypervisor</li>
+      </ul>
+    </li>
+    <li> Back in 2006 we had:
       <ul>
       <li> a small team of smart, pragmatic people</li>
       <li> an existing codebase in C/C++/python</li>
@@ -95,13 +127,13 @@ let slides = [
   content= <:html<
     <h3>Why did we choose OCaml??!</h3>
     <ul>
-    <li> We knew we would spend too much time
+    <li> We knew we were spending <b>waaaay</b> too much time
       <ul>
       <li> tediously parsing strings (protocols, CLIs, config files)</li>
       <li> tracking down the root cause of memory corruption</li>
       </ul>
     </li>
-    <li> We knew we would spend too little time
+    <li> We knew we were spending <b>waaaay</b> too little time
       <ul>
       <li> building features people wanted</li>
       <li> thinking about any kind of "design" at all</li>
@@ -145,7 +177,7 @@ let slides = [
     </li>
 	<li> first task: create an API document for discussion</li>
     <li> why write a doc when you can generate from a declarative spec?</li>
-    <li> .. and then you can genere code</li>
+    <li> .. and then you can generate code</li>
     </ul>
   >>;
 };
@@ -154,9 +186,10 @@ let slides = [
   content= <:html<
     <h3>Why did we choose OCaml??!</h3>
     <ul>
-    <li> 3 of us had FP experience (standard ML)</li>
+    <li> 3 of us had FP experience (standard ML and then OCaml)</li>
     <li> we knew that we could benefit from
     <ul>
+      <li>algebraic data types</li>
       <li>type-checking</li>
       <li>bounds-checking</li>
     </ul>
@@ -169,6 +202,26 @@ let slides = [
       <li> FFI allows us to drop to C if necessary</li>
     </ul>
     </li>
+    </ul>
+  >>
+};
+{
+  styles=[];
+  content= <:html<
+    <h3>So a document generator was born</h3>
+    <ul>
+    <li> a declarative API spec was converted into pdf (via latex)</li>
+    <li> how did we know the API was actually going to work?
+      <ul>
+      <li>clearly we needed a quick prototype implementation</li>
+      </ul>
+    </li>
+    <li> we quickly generated some API stubs, skeletons</li>
+    <li> we quickly hooked up the skeletons to some "actuators" (xen hypercalls etc)</li>
+    <li> ... </li>
+    <li> the result was pretty impressive! </li>
+    <li> ... </li>
+    <li> shall we move the product over to this new codebase then? </li>
     </ul>
   >>
 };
@@ -206,7 +259,33 @@ $str:dl$ git shortlog -s -n | dedup-users | wc -l
 {
   styles=[];
   content= <:html<
-    <h3>Mirage: the other way</h3>
+    <h3>Talk structure</h3>
+    <ol>
+    <li>XenServer and OCaml: a brief history
+      <ul>
+      <li>What is XenServer? </li>
+      <li> Where is the OCaml? </li>
+      </ul>
+    </li>
+    <li><b>Low-level systems programming in OCaml</b>
+      <ul>
+      <li>OCaml in kernel space;</li>
+      <li>Run OCaml directly on the cloud</li>
+      </ul>
+    </li>
+    <li>Future XenServer architecture
+      <ul>
+      <li>Where OCaml fits</li>
+      </ul>
+    </li>
+  </ol>
+  >>;
+};
+
+{
+  styles=[];
+  content= <:html<
+    <h3>Mirage: OCaml in kernel space</h3>
     <ul>
 	  <li><b>&quot;The cloud is the computer&quot;</b>: now we only need simple block, network packet abstractions at the bottom.</li>
       <li>Protocol libraries, rather than layers
@@ -228,26 +307,6 @@ $str:dl$ git shortlog -s -n | dedup-users | wc -l
         </ul>
       </li>
     </ul>
-  >>
-};
-{ styles=[];
-  content= <:html<
-    <h3>Example: xenstore service</h3>
-    <ul>
-      <li>A critical service on a <tt>xen</tt> host, needed by all control operations.</li>
-      <li>Provides an access-controlled key-value store to all VMs.</li>
-    </ul>
-    <section>
-    <object data="xenstore.svg" type="image/svg+xml">&nbsp;</object>
-    </section>
-  >>
-};
-{ styles=[];
-  content= <:html<
-    <h3>Example: this presentation</h3>
-    <section>
-    <object data="website.svg" type="image/svg+xml">&nbsp;</object>
-    </section>
   >>
 };
 { styles=[];
@@ -281,9 +340,19 @@ $str:dl$ git shortlog -s -n | dedup-users | wc -l
     <p><b>Remember: these binaries are self-contained, containing full network stacks etc.</b></p>
   >>
 };
+
 { styles=[];
   content= <:html<
-    <h3>Building</h3>
+    <h3>Example: this presentation</h3>
+    <section>
+    <object data="website.svg" type="image/svg+xml">&nbsp;</object>
+    </section>
+  >>
+};
+
+{ styles=[];
+  content= <:html<
+    <h3>Building an OCaml kernel is easy</h3>
       <p>Set up <b>opam</b></p>
 <pre class="noprettyprint">
 $str:dl$ git clone https://github.com/OCamlPro/opam
@@ -300,22 +369,106 @@ $str:dl$ opam --yes install mirage mirage-net cohttp mirage-fs
      <p>You're ready to go!</p>
   >>
 };
+{
+  styles=[];
+  content= <:html<
+    <h3>Talk structure</h3>
+    <ol>
+    <li>XenServer and OCaml: a brief history
+      <ul>
+      <li>What is XenServer? </li>
+      <li> Where is the OCaml? </li>
+      </ul>
+    </li>
+    <li>Low-level systems programming in OCaml
+      <ul>
+      <li>OCaml in kernel space;</li>
+      <li>Run OCaml directly on the cloud</li>
+      </ul>
+    </li>
+    <li><b>Future XenServer architecture</b>
+      <ul>
+      <li>Where OCaml fits</li>
+      </ul>
+    </li>
+  </ol>
+  >>;
+};
+
+{ styles=[];
+  content= <:html<
+    <h3>"Project Windsor": disaggregating domain 0</h3>
+    <ul>
+      <li> Isolate components in (small) VMs e.g.
+        <ul>
+        <li> Individual device drivers (NIC x; HBA y)</li>
+        <li> Individual services (per-VM hardware emulators</li>
+        </ul>
+      </li>
+      <li> <b>Increase reliability:</b> Monitor, crash and restart on failure (Erlang-style)</li>
+      <li> <b>Better scalability:</b> Dynamically scale-out across NUMA nodes</li>
+      <li> <b>More secure:</b> Reduce size of the Trusted Computing Base</li>
+    </ul>
+  >>
+};
+
+{ styles=[];
+  content= <:html<
+    <h3>"Project Windsor": where OCaml helps</h3>
+    <ul>
+      <li> The same code can be run in user-space (as normal) or kernel-space (via Mirage)
+        <ol>
+        <li>Debug in user-space, deploy in kernel-space</li>
+        </ol>
+      </li>
+      <li> Only include the libraries you actually need
+        <ol>
+        <li> Low memory footprint</li>
+        <li> Extremely fast boot</li>
+        <li> Helps keep the TCB small</li>
+        </ol>
+      </li>
+      <li> High performance
+        <ol>
+        <li> No layers of legacy code </li>
+        <li> No need for separate kernel and user-space within a Mirage VM</li>
+        </ol>
+      </li>
+    </ul>
+  >>
+};
+
+
+{ styles=[];
+  content= <:html<
+    <h3>Example: xenstore service</h3>
+    <ul>
+      <li>A critical service on a <tt>xen</tt> host, needed by all control operations.</li>
+      <li>Provides an access-controlled key-value store to all VMs.</li>
+    </ul>
+    <section>
+    <object data="xenstore.svg" type="image/svg+xml">&nbsp;</object>
+    </section>
+  >>
+};
+
 { styles=[];
   content= <:html<
     <h3>Summary</h3>
     <ul>
-      <li>Building apps with mirage is liberating and fun
-        <ul>
-          <li>I even enjoyed debugging the TCP stack last weekend</li>
-          <li>It makes kernel programming less of a &quot;black art&quot;</li>
-        </ul>
+      <li> using higher-level tools like OCaml, helps us <b>generate more value</b> for the company
+        <ol>
+        <li> I claim the complexity of our software is due to the problem domain, not the programming language(s)</li>
+        <li> You can think of OCaml as "a better C" (if that helps) </li>
+        </ol>
       </li>
-      <li>Give <b>opam</b> a try</li>
-      <li>Be explicit about the interfaces your app needs: do you need all of Unix or just a network stack?
-        <ul>
-          <li> As well as being mirage-friendly, this also makes your app easier to test through &quot;mocks&quot;</li>
-        </ul>
+      <li> virtualization allows us to rethink how our OSes and applications are structured
+      <ol>
+        <li>Remember Rob Pike's "Systems Software Research is Irrelevant"?</li>
+        <li>OCaml in kernel mode is liberating and fun</li>
+      </ol>
       </li>
+      <li><b>Citrix is a great company</b> - speak to me to find out more</li>
     </ul>
   >>
 };
